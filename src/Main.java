94c8da12f48e17fc7c013d569fda26087eae3ff8
Fahad-78 <fahadsorkar23@gmail.com>
2025-10-08 03:38:43 +0600

    first java program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args){
+
+        //This is my first java program
+
+        /*
+        This is a
+        multi line comment
+         */
+
         System.out.println("I like Pizza!");
+        System.out.println("It's really good");
+        System.out.println("Buy me pizza!");
     }
 }
